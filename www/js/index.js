26a019ec83e25b53b8ec5642b960eafb95bbdf7d
CptSpaceToaster <Lugia978@yahoo.com>
2015-08-02 10:28:35 -0400

    some hacking, and button css

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,12 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
+    onPlayGame: function() {
+        var ref = window.location="http://www.playinitium.com/login.jsp"
+        //var ref = window.open('http://www.playinitium.com/login.jsp', '_blank', 'location=no')
+    },
+
     onDeviceReady: function() {
-        var ref = window.open('http://www.playinitium.com/login.jsp', '_blank', 'location=no')
+        //var ref = window.open('http://www.playinitium.com/login.jsp', '_blank', 'location=no')
     }
 };

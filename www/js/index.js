081f82f19422a9566f50696b4f6ce3ed5b086a93
Conor Winchcombe <cpwinchcombe@hotmail.com>
2015-09-05 01:14:12 +0100

    More resizing

--- www/js/index.js
+++ www/js/index.js
@@ -8,9 +8,9 @@ var app = {
     },
 
     onPlayGame: function() {
-        var ref = window.location = "http://www.playinitium.com/login.jsp"
+        //var ref = window.location = "http://www.playinitium.com/login.jsp"
+        var ref = window.open('http://www.playinitium.com/login.jsp', '_blank', 'location=no,')
         ref.resizeTo(window.innerHeight, window.innerWidth)
-        //var ref = window.open('http://www.playinitium.com/login.jsp', '_blank', 'location=no,')
     },
 
     onDeviceReady: function() {

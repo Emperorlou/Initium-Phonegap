49249c21681531b747d3cde63104773e76e9d2f4
Conor Winchcombe <cpwinchcombe@hotmail.com>
2015-09-14 19:33:57 +0100

    I dont even know what this will do

--- www/js/index.js
+++ www/js/index.js
@@ -9,8 +9,8 @@ var app = {
 
     onPlayGame: function() {
         //var ref = window.location = "http://www.playinitium.com/login.jsp"
-        var main_window = window.open('http://www.playinitium.com/main.jsp', '_self', 'location=no, zoom=no');
-        main_window.resizeTo(window.innerHeight, window.innerWidth);
+        var main_window = window.open('http://www.playinitium.com/main.jsp', '_self', 'location=no,');
+        main_window.resizeTo(window.innerHeight, 728);
     },
 
     onDeviceReady: function() {

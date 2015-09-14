567a8fb2dd18815f1cfcb6ca54c481fd90b3b2ac
Conor Winchcombe <cpwinchcombe@hotmail.com>
2015-09-14 19:11:02 +0100

    More window shenanigans

--- www/js/index.js
+++ www/js/index.js
@@ -9,7 +9,7 @@ var app = {
 
     onPlayGame: function() {
         //var ref = window.location = "http://www.playinitium.com/login.jsp"
-        var main_window = window.open('http://www.playinitium.com/main.jsp', '_system', 'location=no,zoom=no');
+        var main_window = window.open('http://www.playinitium.com/main.jsp', '_blank', 'location=no,zoom=no');
         main_window.resizeTo(window.innerHeight, window.innerWidth);
     },
 

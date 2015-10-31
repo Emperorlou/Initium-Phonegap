f00f373e57a777f4f82e01f8a043a196788a6a7a
Nik <narmstrong@universeprojects.com>
2015-10-30 21:14:44 -0400

    Another experiment with a different plugin.

--- www/js/index.js
+++ www/js/index.js
@@ -8,6 +8,7 @@ var app = {
     },
 
     onPlayGame: function() {
+    	doOpen();
         //var ref = window.location = "http://www.playinitium.com/login.jsp"
        // var ref = window.open('http://www.playinitium.com/main.jsp', '_self', 'location=no,');
     },
@@ -24,3 +25,35 @@ var app = {
         navigator.app.exitApp();
     }
 };
+
+
+function doOpen() {
+
+	var options = {
+		toolbarColor : '#FFFFFF', // Background color of the toolbar in #RRGGBB
+		toolbarHeight : '40',
+		closeButtonText : '< Close',
+		closeButtonSize : '25',
+		closeButtonColor : '#000000',
+		openHidden : false
+	};
+
+	if (window.cordova && cordova.platformId === "android") {
+		var browser = window.inAppBrowserXwalk
+				.open("https://www.playinitium.com/main.jsp", options);
+
+		browser.addEventListener("loadstart", function(url) {
+			console.log(url);
+		});
+
+		browser.addEventListener("loadstop", function(url) {
+			console.log(url);
+		});
+
+		browser.addEventListener("exit", function() {
+			console.log("browser closed");
+		});
+	} else {
+		window.open("https://www.playinitium.com/main.jsp", "_self");
+	}
+}
